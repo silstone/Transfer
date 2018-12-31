8504c648392ab570d316fdc345fe09c817472fd8
silstone <Silstonegroup@gmail.com>
2018-12-31 16:29:09 +0530

    commit all changes

---
+++ Transfer/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Transfer
+//
+//  Created by Silstone on 31/12/18.
+//  Copyright © 2018 Silstone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var mainstr:String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    func updateStr()
+    {
+        mainstr = "transfer"
+    }
+
+
+}
+
---
+++ TransferTests/TransferTests.swift
@@ -0,0 +1,36 @@
+//
+//  TransferTests.swift
+//  TransferTests
+//
+//  Created by Silstone on 31/12/18.
+//  Copyright © 2018 Silstone. All rights reserved.
+//
+
+import XCTest
+@testable import Transfer
+
+class TransferTests: XCTestCase {
+    var myController = ViewController()
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+       
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExampleUpdate() {
+        // This is an example of a functional test case.
+        myController.updateStr()
+        XCTAssertEqual(myController.mainstr, "transfer")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
